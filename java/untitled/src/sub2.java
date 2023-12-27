d8cc63ecff620332e3a11c3b56fec217e0ec7a91
Amobu0 <kdehgogo104@gamil.com>
2023-12-27 16:31:13 +0900

    first all

---
+++ java/untitled/src/sub2.java
@@ -0,0 +1,24 @@
+//import 출처
+import java.util.Scanner;
+
+public class sub2 {
+    public static void main(String[] args) {
+        System.out.print("숫자 입력 : ");
+
+        //사용자 입력
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        System.out.println("입력한 숫자: " + num);
+
+        switch (num % 2) {
+            case 0:
+                System.out.println("num은 짝수입니다.");
+                break;
+            case 1:
+                System.out.println("num은 홀수입니다.");
+                break;
+        }
+
+    }
+}
