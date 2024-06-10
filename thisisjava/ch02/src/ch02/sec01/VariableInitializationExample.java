8711c1799e3b47d1cc3553a07166afdb8e42be5d
Amobu0 <kdehgogo104@gmail.com>
2024-06-10 12:52:50 +0900

    java

---
+++ thisisjava/ch02/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        //변수 value 변수
+        int value;
+
+        //연산 결과를 변수 result의 초기값으로 대입
+
+//        int result = value +10;
+
+//        System.out.println("result = " + result);
+    }
+}
