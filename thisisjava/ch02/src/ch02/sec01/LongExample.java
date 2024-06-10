b447d95c196c88bab0ff534cf211098b4cfa66df
Amobu0 <kdehgogo104@gmail.com>
2024-06-10 15:25:03 +0900

    type int

---
+++ thisisjava/ch02/src/ch02/sec01/LongExample.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20L;
+        //컴파일러는 int로 간주하기 때문에 에러 발생
+        //long var3 = 100000000000000;
+        long var4 = 100000000000000L;
+
+    }
+}
