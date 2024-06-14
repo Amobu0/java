9f4bcdfcf7f5242889c607ff3ee21df96f3da6d0
Amobu0 <kdehgogo104@gmail.com>
2024-06-14 17:39:55 +0900

    ch07 finish

---
+++ thisisjava/cch07/src/ch07/SmartPhoneExample.java
@@ -0,0 +1,22 @@
+package ch07;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+
+        SmartPhone myPhone = new SmartPhone("갤럭시", "흰색");
+
+        System.out.println("model: " + myPhone.model);
+        System.out.println("color: " + myPhone.color);
+
+        System.out.println("와이파이 상태:" + myPhone.wifi);
+
+        myPhone.bell();
+        myPhone.sendVoice("여보세요");
+        myPhone.receiveVoice("안녕하세요! 저는 홍길동입니다.");
+        myPhone.sendVoice("네 반갑습니다.");
+        myPhone.hangup();
+
+        myPhone.setWifi(true);
+        myPhone.internet();
+    }
+}
